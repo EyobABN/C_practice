9704719b3ac88277113b7ef10195303deb82f8c4
EyobABN <eyob.alamerew.belay@gmail.com>
2022-05-06 18:56:55 +0300

    Add shell2.c

---
+++ shell2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[])
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+	ssize_t one = 1;
+	int i;
+	pid_t child_pid = 1;
+	int status;
+
+	while (1)
+	{
+		child_pid = fork();
+		if (child_pid == 0)
+		{
+			printf("#cisfun ");
+			nread = getline(&line, &len, stdin);
+			line[nread - one] = '\0';
+			argv[1] = line;
+			argv[2] = NULL;
+			argc = 2;
+			for (i = 0; i < argc; i++)
+			{
+				argv[i] = argv[i + 1];
+			}
+			execve(argv[0], argv, NULL);
+		}
+		else
+		{
+			wait(&status);
+		}
+	}
+	return (0);
+}
